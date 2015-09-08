bfea1c00b6d5368beaa076882012ef84c5e81086
Tim Seed <tim@sy-edm.com>
2015-09-08 11:26:31 +0400

    Basic Version Working

--- t.py
+++ t.py
@@ -1,4 +1,33 @@
 #http://it-ebooks-api.info/v1/
 #The API is   http://it-ebooks-api.info/v1/search/php {Subject}
 
+import pycurl
+from StringIO import StringIO
+import json
+import pprint
 
+URLMAIN="http://it-ebooks-api.info/v1/search/"
+
+def GetSubject(sub):
+  URL=URLMAIN+sub
+  response=StringIO()
+  c=pycurl.Curl()
+  c.setopt(c.URL,URL)
+  c.setopt(c.WRITEDATA,response)
+  c.perform()
+  c.close()
+  jd=json.loads(response.getvalue())
+  total_hits=int(jd[u'Total'])
+  #print ("%s has %d hits "%(sub,total_hits))
+  #pprint.pprint(jd)
+  return total_hits,jd
+
+def BookInfo(search):
+  hits,data=GetSubject(search)
+  for page in range (1,hits/10+1):
+    hits,data=GetSubject(search+"/page/"+str(page))
+    for b in data[u'Books']:
+       print ("%-15s %s")%(b[u'isbn'],b[u'Title'])
+
+
+BookInfo("Python")
